d2391d4f2635ab1e17ff69072de5b93a209e4a26
1felipemartins1 <14felipemartins08@gmail.com>
2024-05-19 11:59:04 -0300

    classe livro e usuario

--- index.py
+++ index.py
@@ -1,5 +1,44 @@
-def add(a, b):
-    return a + b
+class Livro:
+    def __init__(self, nome, id, em_estoque=True):
+        self.nome = nome
+        self.id = id
+        self.em_estoque = em_estoque
 
-def subtract(a, b):
-    return a - b
+class Usuario:
+    _ids_existentes = set()
+
+    def __init__(self, id, nome=""):
+        if id in Usuario._ids_existentes:
+            raise ValueError("ID já existe. Por favor, use um ID único.")
+        self.id = id
+        self.nome = nome
+        self.livros = []
+        Usuario._ids_existentes.add(id)
+
+    def get_nome(self):
+        return self._nome
+
+    def get_id(self):
+        return self._id
+    
+    def adiciona_livro(self, livro):
+        
+        if isinstance(livro, Livro):
+            self.livros.append(livro)
+        else:
+            raise ValueError("O objeto adicionado deve ser uma instância da classe Livro.")
+
+    def remover_livro(self, livro_id):
+        
+        for livro in self.livros:
+            if livro.id == livro_id:
+                self.livros.remove(livro)
+                return True
+        return False
+
+    def verifica_livro(self, livro_id):
+        
+        for livro in self.livros:
+            if livro.id == livro_id:
+                return True
+        return False
