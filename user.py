f9151877f56a0a06aa6e71c91db833a14fadff5e
1felipemartins1 <14felipemartins08@gmail.com>
2024-05-20 18:27:31 -0300

    atualização do user e da library

--- user.py
+++ user.py
@@ -8,7 +8,40 @@ class User:
         self.is_admin = is_admin
         self.current_loans_count = 0
 
+    def get_nome(self):
+        return self.name
+
+    def get_CPF(self):
+        return self.CPF
+
+    def get_email(self):
+        return self.email 
     
+    def get_password(self):
+        return self.password 
+    
+    def get_is_admin(self):
+        return self.is_admin
+    
+    def create_user(self, CPF, name, email, password, is_admin):
+        if self.is_admin:
+            return User(CPF, name, email, password, is_admin)
+        else:
+            raise PermissionError("Apenas administradores podem criar novos usuários.")
+
+    def add_book(self, book):
+        self.loans.append(book)
+        self.current_loans_count += 1
+
+    def remove_book(self, book_id):
+        for book in self.loans:
+            if book.id == book_id:
+                self.loans.remove(book)
+                self.current_loans_count -= 1
+                return True
+        return False
 
 
+    def has_books(self):
+        return self.current_loans_count > 0    
     
