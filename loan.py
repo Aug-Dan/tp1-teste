75819a9d0fc3ae4cc180176c7c61bf7c0dabe195
Leonardo Magalhães <106030073+Le0-1@users.noreply.github.com>
2024-05-21 17:35:42 -0300

    chore: Refactor Loan class to include loan period and renewal functionality

--- loan.py
+++ loan.py
@@ -1,17 +1,40 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from book import Book
 from user import User
+import holidays
+
+MAX_RENEWALS = 3
+LOAN_PERIOD_DAYS = 14
 
 class Loan:
 
-    def __init__(self, user, book, loan_date, renewal_credits):
+    def __init__(self, user, book, loan_date):
         if not isinstance(loan_date, datetime):
             raise TypeError("loan_date deve ser do tipo datetime")
 
         self.user = user
         self.book = book
-        self.loan_date = datetime.now()
-        self.return_date = # Calcula uma semana no futuro
-        self.renewal_credits = 3
+        self.loan_date = loan_date
+        self.due_date = loan_date + timedelta(days=LOAN_PERIOD_DAYS)
+        self.renewals = 0
+        
+    def calculate_due_date(self):
+        due_date = self.due_date + timedelta(days=LOAN_PERIOD_DAYS)
+        
+        # Verifica se a data de vencimento é um fim de semana ou feriado
+        while due_date.weekday() >= 5 or due_date in holidays.Brazil():
+            due_date += timedelta(days=1)
+        
+        return due_date
+
+    def renew(self):
+        if (self.renewals < MAX_RENEWALS):
+            self.due_date = self.calculate_due_date()
+            self.renew += 1
+        else:
+            raise ValueError("Número máximos de renovações atingido")
+    
+    def __repr__(self):
+        return f"Loan({self.user}, {self.book}, {self.due_date}, {self.renewals} renewals)"            
 
     
