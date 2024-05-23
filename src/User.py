d26e9fcad57a142d54bb2503a25464048c4efa08
1felipemartins1 <14felipemartins08@gmail.com>
2024-05-23 16:07:00 -0300

    descomentei os adds

--- src/User.py
+++ src/User.py
@@ -33,10 +33,10 @@ class User:
 
     # Métodos de 
 
-    # def add_book(self, book):
-    #     self.current_loans_count += 1
+    def add_book(self, book):
+         self.current_loans_count += 1
 
-    # def remove_book(self, book):
-    #     if self.current_loans_count == 0:
-    #         raise ValueError("A contagem de empréstimos já é zero. Não é possível remover mais livros.")
-    #     self.current_loans_count -= 1
+    def remove_book(self, book):
+        if self.current_loans_count == 0:
+            raise ValueError("A contagem de empréstimos já é zero. Não é possível remover mais livros.")
+        self.current_loans_count -= 1
