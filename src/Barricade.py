1a18a32efdfbe6f04504aafb7fa0fcc5ce7d7d8c
Leonardo Magalhães <106030073+Le0-1@users.noreply.github.com>
2024-05-23 15:15:41 -0300

    chore: Refactor CPF validation logic and add email validation

--- src/Barricade.py
+++ src/Barricade.py
@@ -1,4 +1,3 @@
-from Book import Book 
 import re
 
 VALID_CPF_LEN = 11
@@ -7,23 +6,21 @@ class Barricade:
     class InputValidator:
         @staticmethod
         def is_valid_cpf(cpf: int) -> bool:
-            # Converte o CPF para string
-            cpf = str(cpf)
-
-            # Um CPF válido deve ter 11 dígitos
-            if len(cpf) != VALID_CPF_LEN:
+            # Um CPF válido deve ter 11 dígitos e não ser uma sequência repetida
+            cpf_str = str(cpf).zfill(VALID_CPF_LEN)
+            if len(cpf_str) != VALID_CPF_LEN or len(set(cpf_str)) == 1:
                 return False
 
             # Calcula o primeiro dígito verificador
-            sum_of_products = sum((10 - i) * int(digit) for i, digit in enumerate(cpf[:9]))
+            sum_of_products = sum((10 - i) * int(cpf_str[i]) for i in range(9))
             expected_first_check_digit = (sum_of_products * 10 % 11) % 10
 
             # Calcula o segundo dígito verificador
-            sum_of_products = sum((11 - i) * int(digit) for i, digit in enumerate(cpf[:10]))
+            sum_of_products = sum((11 - i) * int(cpf_str[i]) for i in range(10))
             expected_second_check_digit = (sum_of_products * 10 % 11) % 10
 
             # O CPF é válido se os dígitos verificadores calculados correspondem aos dígitos verificadores reais
-            return cpf[-2:] == str(expected_first_check_digit) + str(expected_second_check_digit)
+            return cpf_str[9] == str(expected_first_check_digit) and cpf_str[10] == str(expected_second_check_digit)
 
         @staticmethod
         def is_valid_password(password: str) -> bool:
@@ -31,11 +28,22 @@ class Barricade:
             if " " in password:
                 return False
 
-            # A senha deve ter pelo menos uma letra maiúscula, uma letra minúscula e um caractere especial
-            if re.search("[A-Z]", password) and re.search("[a-z]", password) and re.search("[!@#$%^&*()]", password):
+            # A senha deve ter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula e um caractere especial
+            if (
+                len(password) >= 8 and
+                re.search("[A-Z]", password) and
+                re.search("[a-z]", password) and
+                re.search("[!@#$%^&*()_+=-]", password)
+            ):
                 return True
 
             return False
+        
+        @staticmethod
+        def is_valid_email(email: str) -> bool:
+            # Expressão regular para validar o formato do email
+            email_regex = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+            return re.match(email_regex, email) is not None
 
         @staticmethod
         def is_string(input) -> bool:
